166f73425355eda3eb03268f98154fd2ca0cdbb6
dennis.adrian <adriangbetancur@gmail.com>
2021-05-04 22:37:40 -0400

    Add button for changing regions in the by-region component and make the buttons change class dynamically

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -4,13 +4,28 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+  regions = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  activateRegion(region: string) {
+    this.activeRegion = region;
+
+    //TODO: make service call
   }
 
+  getClassCSS(region: string) {
+    return region === this.activeRegion
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
 }
